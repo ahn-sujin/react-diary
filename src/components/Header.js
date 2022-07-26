485147ccdda21bc2c5200d4831ff64bf2a02bb9a
asj <asj9674@gmail.com>
2022-07-26 18:59:21 +0900

    navigate연결

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,20 @@
+import { useNavigate } from 'react-router-dom';
 import './header.css';
 
 const Header = () => {
+    const navigate = useNavigate();
+    const goToList = () => {
+        navigate('/');
+    }
+    const goToWrite = () => {
+        navigate('/write')
+    }
+
     return(
         <header>
             <div className="inner">
-                <h1 className="logo">일기장</h1>
-                <button className="btn btn_white">일기 쓰기</button>
+                <h1 className="logo" onClick = {goToList}>일기장</h1>
+                <button className="btn btn_white" onClick = {goToWrite}>일기 쓰기</button>
             </div>  
         </header>
     )
