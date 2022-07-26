44bf28aab87f01148e3ff159d718cf88a97dac5c
asj <asj9674@gmail.com>
2022-07-26 21:00:10 +0900

    리스트 페이지 db연동 완료

--- src/features/List/List.js
+++ src/features/List/List.js
@@ -1,29 +1,36 @@
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './list.css';
 
 const List = () => {
+    const [ diary, setDiary ] = useState([]);
+
     const navigate = useNavigate();
     const goToDetail = () => {
         navigate('/detail');
     }
 
+    useEffect(() => {
+        fetch('http://localhost:4000/diary')
+        .then(res => res.json())
+        .then(data => setDiary(data));
+    })
+
     return(
         <div className="container">
             <ul className="list_area">
-                <li className="box" onClick={goToDetail}>
-                    <p className="date">2022.07.25</p>
-                    <p className="title">별 헤는 밤</p>
-                    <p className="contents">이런 아침이 말 프랑시스 동경과 오면 봅니다. 언덕 가득 어머니, 않은 까닭입니다. 하나에 멀듯이, 가난한 많은 이름을 나의 없이 청춘이 버리었습니다. 보고, 차 계집애들의 비둘기, 무엇인지 가득 이름과, 봅니다. 말 노새, 했던 봅니다. 별들을 추억과 하나에 프랑시스 경, 하나의 오면 있습니다. 별 멀리 속의 하나의 잠, 듯합니다. 슬퍼하는 동경과 시인의 옥 하나에 무성할 있습니다. 내 어머님, 같이 북간도에 멀리 딴은 그리워 가득 별 까닭입니다. 이네들은 새워 하나의 마리아 까닭입니다. 시인의 무성할 우는 소학교 새워 위에도 말 마디씩 봅니다.이런 아침이 말 프랑시스 동경과 오면 봅니다. 언덕 가득 어머니, 않은 까닭입니다. 하나에 멀듯이, 가난한 많은 이름을 나의 없이 청춘이 버리었습니다. 보고, 차 계집애들의 비둘기, 무엇인지 가득 이름과, 봅니다. 말 노새, 했던 봅니다. 별들을 추억과 하나에 프랑시스 경, 하나의 오면 있습니다. 별 멀리 속의 하나의 잠, 듯합니다. 슬퍼하는 동경과 시인의 옥 하나에 무성할 있습니다. 내 어머님, 같이 북간도에 멀리 딴은 그리워 가득 별 까닭입니다. 이네들은 새워 하나의 마리아 까닭입니다. 시인의 무성할 우는 소학교 새워 위에도 말 마디씩 봅니다.이런 아침이 말 프랑시스 동경과 오면 봅니다. 언덕 가득 어머니, 않은 까닭입니다. 하나에 멀듯이, 가난한 많은 이름을 나의 없이 청춘이 버리었습니다. 보고, 차 계집애들의 비둘기, 무엇인지 가득 이름과, 봅니다. 말 노새, 했던 봅니다. 별들을 추억과 하나에 프랑시스 경, 하나의 오면 있습니다. 별 멀리 속의 하나의 잠, 듯합니다. 슬퍼하는 동경과 시인의 옥 하나에 무성할 있습니다. 내 어머님, 같이 북간도에 멀리 딴은 그리워 가득 별 까닭입니다. 이네들은 새워 하나의 마리아 까닭입니다. 시인의 무성할 우는 소학교 새워 위에도 말 마디씩 봅니다.이런 아침이 말 프랑시스 동경과 오면 봅니다. 언덕 가득 어머니, 않은 까닭입니다. 하나에 멀듯이, 가난한 많은 이름을 나의 없이 청춘이 버리었습니다. 보고, 차 계집애들의 비둘기, 무엇인지 가득 이름과, 봅니다. 말 노새, 했던 봅니다. 별들을 추억과 하나에 프랑시스 경, 하나의 오면 있습니다. 별 멀리 속의 하나의 잠, 듯합니다. 슬퍼하는 동경과 시인의 옥 하나에 무성할 있습니다. 내 어머님, 같이 북간도에 멀리 딴은 그리워 가득 별 까닭입니다. 이네들은 새워 하나의 마리아 까닭입니다. 시인의 무성할 우는 소학교 새워 위에도 말 마디씩 봅니다.</p>
-                    <div className="tag_wrap">
-                        <div className="tag">테스트</div>
-                        <div className="tag">테스트</div>
-                        <div className="tag">테스트</div>
-                        <div className="tag">테스트</div>
-                        <div className="tag">테스트</div>
-                        <div className="tag">테스트</div>
-                        <div className="tag">테스트</div>
-                    </div>
-                </li>
+                {diary.map((data) => (
+                    <li className="box" onClick={goToDetail}>
+                        <p className="date">{data.date}</p>
+                        <p className="title">{data.title}</p>
+                        <p className="contents">{data.contents}</p>
+                        <ul className="tag_wrap">
+                            {data.tag.map((tag) => (
+                                <li className="tag">{tag.name}</li>
+                            ))}
+                        </ul>
+                    </li>
+                ))}
             </ul>
         </div>  
     )
